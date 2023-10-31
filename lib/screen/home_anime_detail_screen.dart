a83028323512b06e00020f39d43be0b20169d4be
EmmanuelSalcido <emmanuelem2000@hotmail.com>
2023-10-30 18:59:41 -0600

    bug solucionado de trailer de home

--- lib/screen/home_anime_detail_screen.dart
+++ lib/screen/home_anime_detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:anime_app/screen/anime_detail_screen.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class HomeAnimeDetailScreen extends StatefulWidget {
   final String animeTitle;
@@ -12,27 +12,36 @@ class HomeAnimeDetailScreen extends StatefulWidget {
 }
 
 class _HomeAnimeDetailScreenState extends State<HomeAnimeDetailScreen> {
+  late YoutubePlayerController _controller;
+  bool isVideoVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    final trailerUrl = widget.animeDetails?['trailer']?['embed_url'] as String?;
+    if (trailerUrl != null) {
+      _controller = YoutubePlayerController(
+        initialVideoId: YoutubePlayer.convertUrlToId(trailerUrl)!,
+        flags: YoutubePlayerFlags(
+          autoPlay: false,
+          mute: false,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final imageUrl = widget.animeDetails?['images']?['jpg']?['large_image_url'];
     final synopsis = widget.animeDetails?['synopsis'];
+    final ranking = widget.animeDetails?['rank'] ?? 'Sin ranking';
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          widget.animeTitle,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
         backgroundColor: Colors.black,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        title: Text(
+          'Animes de Temporada',
+          style: TextStyle(color: Colors.white),
         ),
       ),
       body: SingleChildScrollView(
@@ -40,6 +49,7 @@ class _HomeAnimeDetailScreenState extends State<HomeAnimeDetailScreen> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
+              // Imagen del anime
               if (imageUrl != null)
                 Image.network(imageUrl)
               else
@@ -47,19 +57,58 @@ class _HomeAnimeDetailScreenState extends State<HomeAnimeDetailScreen> {
 
               SizedBox(height: 16),
 
+              // Título del anime
               Text(
                 widget.animeTitle,
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
+
               SizedBox(height: 16),
+
+              // Ranking
+              Text(
+                'Ranking: $ranking',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+
+              SizedBox(height: 16),
+
+              // Sinopsis
               Text(
                 synopsis ?? 'Sin sinopsis disponible',
                 style: TextStyle(fontSize: 16),
               ),
+
+              SizedBox(height: 16),
+
+              // Botón para ver u ocultar el video
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isVideoVisible = !isVideoVisible;
+                  });
+                },
+                child: Text(isVideoVisible ? 'Ocultar Tráiler' : 'Ver Tráiler'),
+              ),
+
+              SizedBox(height: 16),
+
+              // Reproductor de YouTube para el tráiler si está disponible
+              if (isVideoVisible)
+                YoutubePlayer(
+                  controller: _controller,
+                  showVideoProgressIndicator: true,
+                ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
