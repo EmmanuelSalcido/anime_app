422a0ab6a69ae9af3718f87186eb15e4ced7919e
EmmanuelSalcido <emmanuelem2000@hotmail.com>
2023-11-05 22:34:45 -0600

    mejora visual de resultados

--- lib/screen/Resultados_screen.dart
+++ lib/screen/Resultados_screen.dart
@@ -1,6 +1,5 @@
 import 'package:anime_app/screen/explore_detail_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class SearchResultsScreen extends StatelessWidget {
   final List<dynamic> searchResults;
@@ -10,37 +9,50 @@ class SearchResultsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Resultados de Búsqueda'),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(56.0), // Altura de la AppBar personalizada
+        child: AppBar(
+          backgroundColor: Colors.black, // Fondo negro
+          title: Text('Resultados de Búsqueda'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop(); // Para regresar a la pantalla anterior
+            },
+          ),
+        ),
       ),
       body: _buildSearchResults(context),
     );
   }
 
   Widget _buildSearchResults(BuildContext context) {
-    return ListView.builder(
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
       itemCount: searchResults.length,
       itemBuilder: (context, index) {
         final animeData = searchResults[index];
         final animeTitle = animeData['title'];
-        final animeSynopsis = animeData['synopsis']; // Sinopsis del anime
-        final trailerUrl = animeData['trailer_url']; // URL del trailer (si está disponible)
-       final imageUrl = animeData['images']['jpg']['large_image_url']; // URL de la imagen
+        final animeSynopsis = animeData['synopsis'];
+        final trailerUrl = animeData['trailer_url'];
+        final imageUrl = animeData['images']['jpg']['large_image_url'];
 
-        return ListTile(
-          title: Text(animeTitle),
+        return GestureDetector(
           onTap: () {
             Navigator.of(context).push(
               MaterialPageRoute(
                 builder: (context) => ExploreDetailScreen(
                   animeTitle: animeTitle,
-                  synopsis: animeSynopsis, // Pasamos la sinopsis
-                  trailerUrl: trailerUrl, // Pasamos la URL del trailer
-                  imageUrl: imageUrl, // Pasamos la URL de la imagen
+                  synopsis: animeSynopsis,
+                  trailerUrl: trailerUrl,
+                  imageUrl: imageUrl,
                 ),
               ),
             );
           },
+          child: Image.network(imageUrl),
         );
       },
     );
